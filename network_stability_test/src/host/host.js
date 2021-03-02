6ab4aa2a7f511ece294e2a5f55b4f75e9c233b2b
xjl <995363794@qq.com>
2021-03-02 17:04:59 +0800

    add stb1 control function

--- network_stability_test/src/host/host.js
+++ network_stability_test/src/host/host.js
@@ -1,11 +1,19 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://47.101.199.165'); //连接到服务端
 
-
 var successPackCount = 0;
 var allPackCount = 0;
 
 var testDeviceName = "706_a7600"
+const { crc16modbus } = require('crc');
+const config = require('config');
+const port = config.get('netwoek_stability_test-modbusSetting').port;
+const option = config.get('netwoek_stability_test-modbusSetting').option;
+const SerialPort = require('serialport');
+const device = new SerialPort(port, option);
+
+
+var downLinkFlag = 1;
 
 client.on('connect', function () {
     console.log(`connext is ok`)
@@ -34,11 +42,40 @@ client.on('message', function (topic, message) {
     }
 });
 
+function STB1_on() {
+    let crc = Buffer.alloc(2);
+    payload = Buffer.from([0x01, 0x05, 0x00, 0x01, 0xff, 0x00]);
+    crc.writeUInt16LE(crc16modbus(payload), 0);
+    device.write(Buffer.concat([payload, crc]));
+}
+
+function STB1_off() {
+    let crc = Buffer.alloc(2);
+    payload = Buffer.from([0x01, 0x05, 0x00, 0x01, 0x00, 0x00]);
+    crc.writeUInt16LE(crc16modbus(payload), 0);
+    device.write(Buffer.concat([payload, crc]));
+}
+
 setInterval(() => {
-    allPackCount += 1;
-    client.publish("pc_test_tools/network_stability_test/downLink/706_a7600", JSON.stringify({ downLinkCount: allPackCount }))
+    if (downLinkFlag == 1) {
+        allPackCount += 1;
+        client.publish("pc_test_tools/network_stability_test/downLink/706_a7600", JSON.stringify({ downLinkCount: allPackCount }))
+    }
 }, 1000);
 
 setInterval(() => {
-    console.log(`${new Date().toLocaleString()} all pack num:${allPackCount} ,success pack num:${successPackCount} ,percentage of success: ${successPackCount *100/ allPackCount}% `)
+    console.log(`${new Date().toLocaleString()} all pack num:${allPackCount} ,success pack num:${successPackCount} ,percentage of success: ${successPackCount * 100 / allPackCount}% `)
 }, 10000);
+
+setInterval(() => {
+    STB1_off();
+    downLinkFlag = 0;
+    console.log(`${new Date().toLocaleString()} off `)
+    setTimeout(() => {
+        STB1_on();
+        console.log(`${new Date().toLocaleString()} on `)
+        setTimeout(() => {
+            downLinkFlag = 1;
+        }, 1000 * 60 * 5);//5分钟 开机延迟
+    }, 1000 * 60 * 2);//2分钟 放电延迟
+}, 1000 * 60 * 10);//10分钟 断电循环周期
