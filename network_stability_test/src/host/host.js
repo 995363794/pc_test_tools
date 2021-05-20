b4e1e8ddb94d00be6c6f613d7cb61403ee8563e8
xjl <995363794@qq.com>
2021-05-20 16:58:56 +0800

    1. update README

--- network_stability_test/src/host/host.js
+++ network_stability_test/src/host/host.js
@@ -3,6 +3,8 @@ var client = mqtt.connect('mqtt://47.101.199.165'); //连接到服务端
 
 var successPackCount = 0;
 var allPackCount = 0;
+var oneSuccessPackCount = 0;
+var oneAllPackCount = 0;
 
 var testDeviceName = "706_a7600"
 const { crc16modbus } = require('crc');
@@ -14,6 +16,9 @@ const device = new SerialPort(port, option);
 
 
 var downLinkFlag = 1;
+
+var runFlag = 1;
+var lodUpLinkData;
 
 client.on('connect', function () {
     console.log(`connext is ok`)
@@ -31,10 +36,12 @@ client.on('message', function (topic, message) {
     if (topic.indexOf(`upLink/${testDeviceName}`) != -1) {
         try {
             var data = JSON.parse(message);
+            lodUpLinkData = data;
             // console.log(data)
             // console.log(allPackCount)
             if (data.ack == 1 && data.upCount == allPackCount) {
                 successPackCount += 1;
+                oneSuccessPackCount += 1;
             }
         } catch (error) {
             console.log(error)
@@ -59,23 +66,35 @@ function STB1_off() {
 setInterval(() => {
     if (downLinkFlag == 1) {
         allPackCount += 1;
+        oneAllPackCount += 1;
         client.publish("pc_test_tools/network_stability_test/downLink/706_a7600", JSON.stringify({ downLinkCount: allPackCount }))
     }
 }, 1000);
 
 setInterval(() => {
-    console.log(`${new Date().toLocaleString()} all pack num:${allPackCount} ,success pack num:${successPackCount} ,percentage of success: ${successPackCount * 100 / allPackCount}% `)
+    console.log(`lodUpLinkData.upCount :${lodUpLinkData.upCount}`)
+    if (lodUpLinkData.upCount == allPackCount) {
+        runFlag = 1;
+    } else if ((oneAllPackCount - oneSuccessPackCount) >= 100) {
+        runFlag = 0;
+    }
+    console.log(`${new Date().toLocaleString()} all pack:${allPackCount} ,success pack:${successPackCount} ,percentage of success: ${successPackCount * 100 / allPackCount}% ,${oneAllPackCount},${oneSuccessPackCount}`)
 }, 10000);
 
 setInterval(() => {
-    STB1_off();
-    downLinkFlag = 0;
-    console.log(`${new Date().toLocaleString()} off `)
-    setTimeout(() => {
-        STB1_on();
-        console.log(`${new Date().toLocaleString()} on `)
+    if (runFlag == 1) {
+        STB1_off();
+        oneSuccessPackCount = 0;
+        oneAllPackCount = 0;
+
+        downLinkFlag = 0;
+        console.log(`${new Date().toLocaleString()} off `)
         setTimeout(() => {
-            downLinkFlag = 1;
-        }, 1000 * 60 * 2);//2分钟 开机延迟
-    }, 1000 * 60 * 2);//2分钟 放电延迟
-}, 1000 * 60 * 60*2);//2小时 断电循环周期
+            STB1_on();
+            console.log(`${new Date().toLocaleString()} on `)
+            setTimeout(() => {
+                downLinkFlag = 1;
+            }, 1000 * 60 * 3);//3分钟 开机延迟
+        }, 1000 * 60 * 2);//2分钟 放电延迟
+    }
+}, 1000 * 60 * 10);//10分钟 断电循环周期
